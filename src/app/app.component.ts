853615f3d6ae0948269ae6d6fe6bacd43db3fd55
hp-pc <navyashree811@gmail.com>
2022-11-04 14:01:48 +0530

    Added : Display Hello Ensure UserName is valid , In case Error display error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,30 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  //uc4
+  //uc5
   title = 'Hello World';
   imageUrl ='../assets/logo.png'
   url="https://www.bridgelabz.com/";
-  userName:String = "";
-
+  userName:string = "";
+  nameError:string = "";
+  
   ngOnInit(): void{
     this.title="Hello World from BridgeLabz"
     }
 
     onClick($event:any) {
-      console.log("Image has been clicked ! ", $event);
+      console.log("Save button is clicked ! ", $event);
       window.open(this.url, "_blank");
+    }
+    
+    onInput($event:any){
+      console.log("Change Event Occurred !", $event.data);
+      const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+      if (nameRegex.test(this.userName)){
+        this.nameError = "";
+        return;
+      }
+    this.nameError = "Name is Incorrect !"
     }
 
   }
